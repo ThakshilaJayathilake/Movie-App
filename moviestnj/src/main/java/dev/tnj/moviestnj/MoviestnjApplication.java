46001024e950bf545e76302ddb4eda17f7a4cbf9
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-03-29 15:59:47 +0530

    spring backend initialize

---
+++ moviestnj/src/main/java/dev/tnj/moviestnj/MoviestnjApplication.java
@@ -0,0 +1,13 @@
+package dev.tnj.moviestnj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviestnjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviestnjApplication.class, args);
+	}
+
+}
---
+++ moviestnj/src/test/java/dev/tnj/moviestnj/MoviestnjApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tnj.moviestnj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviestnjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

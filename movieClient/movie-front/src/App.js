553c0c85791d714de17ad102a4adc77995491748
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-04-12 00:01:24 +0530

    add header

--- movieClient/movie-front/src/App.js
+++ movieClient/movie-front/src/App.js
@@ -1,33 +1,35 @@
-import './App.css';
-import api from './api/axiosConfig';
-import { useState, useEffect } from 'react';
-import Layout from './components/Layout';
-import {Routes, Route} from 'react-router-dom';
-import Home from './components/home/Home';
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   const [movies, setMovies] = useState();
 
-  const getMovies = async() => {
+  const getMovies = async () => {
     try {
-      const response = await api.get("/api/v1/movies")
+      const response = await api.get("/api/v1/movies");
       console.log(response.data);
       setMovies(response.data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   // getMovies function executed when app component first loads
   useEffect(() => {
     getMovies();
-  },[])
+  }, []);
 
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home movies={movies}/>}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
         </Route>
       </Routes>
     </div>
